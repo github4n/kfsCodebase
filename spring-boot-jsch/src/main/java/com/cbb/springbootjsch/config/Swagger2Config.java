5d0a5600fddddcfd16458ea80703805f5e7c5636
kkkfs <kongfanshuo0224@163.com>
2018-12-13 09:46:05 +0800

    newest modify time = 2018/12/13  9:46:04

---
+++ spring-boot-jsch/src/main/java/com/cbb/springbootjsch/config/Swagger2Config.java
@@ -0,0 +1,49 @@
+package com.cbb.springbootjsch.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.collect.Sets;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .produces(Sets.newHashSet("application/json","text/plain"))
+                .consumes(Sets.newHashSet("application/json","text/plain"))
+                .protocols(Sets.newHashSet("http","https"))
+                .apiInfo(apiInfo())
+                .forCodeGeneration(true)
+                .useDefaultResponseMessages(false)
+                .select()
+                // 指定controller存放的目录路径
+                .apis(RequestHandlerSelectors.basePackage("com.cbb"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 文档标题
+                .title("Spring Boot jsch项目的RESTful APIs")
+                // 文档描述
+                .description("联系cbb")
+                .version("v1")
+                .build();
+    }
+}
